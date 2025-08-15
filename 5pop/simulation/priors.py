9c5b15cfa1b83aa9170b168b9eb4944de32c9ab2
Harry Gordon <hlmgordon@hotmail.co.uk>
2025-08-15 12:51:46 +0100

    changed 5pop priors and ran first round sims with point mu/rho

--- 5pop/simulation/priors.py
+++ 5pop/simulation/priors.py
@@ -5,8 +5,30 @@ import scipy.stats
 import pickle
 import pandas as pd
 
-### 25/06/2024
+
+### 25/06/2024 updated 30/06/2025
 priors = {
+    "captive_time": scipy.stats.lognorm(s=0.7, loc=0, scale=np.exp(3)),
+    "div_time": scipy.stats.lognorm(s=0.7, loc=10000, scale=60000),
+    "div_time_dom": scipy.stats.lognorm(s=0.5, loc=500, scale=3000),
+    "div_time_scot": scipy.stats.lognorm(s=0.3, loc=0, scale=3000),
+    "mig_rate_captive": scipy.stats.lognorm(s=1, loc=0, scale=0.07),
+    "mig_rate_scot": scipy.stats.lognorm(s=1, loc=0, scale=0.07),
+    "mig_length_scot": scipy.stats.lognorm(s=0.7, loc=0, scale=np.exp(3)),
+    "pop_size_captive": scipy.stats.lognorm(s=0.6, loc=0, scale=100),
+    "pop_size_domestic_1": scipy.stats.lognorm(s=0.9, loc=0, scale=150000),
+    "pop_size_lyb_1": scipy.stats.lognorm(s=1, loc=0, scale=20000),
+    "pop_size_lyb_2": scipy.stats.lognorm(s=1.5, loc=5000, scale=100000),
+    "pop_size_scot_1": scipy.stats.lognorm(s=1, loc=0, scale=4000),
+    "pop_size_eu_1": scipy.stats.lognorm(s=1.3, loc=0, scale=20000),
+    "pop_size_eu_2": scipy.stats.lognorm(s=1, loc=0, scale=60000)#,
+    #"mutation_rate": scipy.stats.lognorm(s=1, loc=0, scale=1e-8),
+    #"recombination_rate": scipy.stats.lognorm(s=0.5, loc=0.4e-8, scale=9e-9)
+}
+
+
+### 25/06/2024
+priors_2 = {
     "captive_time": scipy.stats.lognorm(s=0.7, loc=0, scale=np.exp(3)),
     "div_time": scipy.stats.lognorm(s=0.7, loc=10000, scale=60000),
     "div_time_dom": scipy.stats.lognorm(s=0.5, loc=500, scale=3000),
